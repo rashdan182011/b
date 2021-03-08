f33101ee067a45a1f09fa89e00f1cee824dc9fe7
rashdan182011 <78985293+rashdan182011@users.noreply.github.com>
2021-03-07 18:53:51 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  var options={
+	  restitution:1.3,
+	  friction:5,
+	  density:1
+  }
+  drawSprites();
+ 
+}
+                   
+
+
